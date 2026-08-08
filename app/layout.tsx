0cb714082fe01e2a03af7c038ba67b0147d5de78
Oluwasusi Stephen <stephanyemmitty@gmail.com>
2026-08-08 07:45:53 +0100

    pushing the latest

--- app/layout.tsx
+++ app/layout.tsx
@@ -173,7 +173,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
-      <body className="min-h-screen flex flex-col antialiased hidden">
+      <body className="min-h-screen flex flex-col antialiased hidden!">
         {children}
         <WhatsAppButton />
       </body>
